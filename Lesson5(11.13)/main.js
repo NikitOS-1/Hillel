63120dea0939e147ff43ef883100f674c0d1c6f4
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-13 21:23:03 +0200

    .

--- Lesson5(11.13)/main.js
+++ Lesson5(11.13)/main.js
@@ -1,7 +1,7 @@
-let arr = [[1, 2, [3, [1, [3, [4, [8]]]]]]];
+// let arr = [[1, 2, [3, [1, [3, [4, [8]]]]]]];
 
-console.log(arr.flat(Infinity));
-const arr2 = new Array();
+// console.log(arr.flat(Infinity));
+// const arr2 = new Array();
 
 // array
 
