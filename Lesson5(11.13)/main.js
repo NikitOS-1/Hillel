5687807e7eab3c64adb674aec13dd663f629bfdd
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-13 20:21:42 +0200

    forEach

--- Lesson5(11.13)/main.js
+++ Lesson5(11.13)/main.js
@@ -27,3 +27,13 @@ const arr2 = new Array();
 
 // // removed равен ["mandarin"]
 // // myFish равен ["angel", "clown", "drum", "sturgeon"]
+
+// Метод forEach() выполняет указанную функцию один раз для каждого элемента в массиве.
+
+// const array1 = ['a', 'b', 'c'];
+
+// array1.forEach((element) => console.log(element));
+
+// Expected output: "a"
+// Expected output: "b"
+// Expected output: "c"
