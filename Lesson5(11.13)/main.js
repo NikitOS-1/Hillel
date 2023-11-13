87887370aa6a57183389fc0031b676d5c5114074
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-11-13 19:49:24 +0200

    array methods

--- Lesson5(11.13)/main.js
+++ Lesson5(11.13)/main.js
@@ -2,3 +2,13 @@ let arr = [[1, 2, [3, [1, [3, [4, [8]]]]]]];
 
 console.log(arr.flat(Infinity));
 const arr2 = new Array();
+
+// array
+
+// .push() - add element in to over array
+
+// .pop() - delete last element in array
+
+// .unshift() - add element in to front array
+
+// .shift() - delete fist element in array
