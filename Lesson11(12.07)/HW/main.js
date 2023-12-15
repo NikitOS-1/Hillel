ab19fda9283af995b62869663fdd944c031298b3
Nikita <69642254+NikitOS-1@users.noreply.github.com>
2023-12-15 16:47:50 +0200

    add lesson 12 and 13

--- Lesson11(12.07)/HW/main.js
+++ Lesson11(12.07)/HW/main.js
@@ -33,3 +33,50 @@
 
 // // А скільки тепер коштує?
 // console.log("Price with sauce: “ + hamburger.calculatePrice());
+
+const STUFFING_CHEESE = { price: 10, calories: 20, name: "cheese" };
+const STUFFING_SALAD = { price: 20, calories: 5, name: "salad" };
+const STUFFING_POTATO = { price: 15, calories: 10, name: "potato" };
+
+const TOPPING_SPICE = { price: 15, calories: 0, name: "spice" };
+const TOPPING_MAYO = { price: 20, calories: 5, name: "mayo" };
+
+class Hamburger {
+  constructor(size, stuffing) {
+    this.size = size;
+    this.stuffing = stuffing;
+    this.toppings = [];
+  }
+
+  addTopping(topping) {
+    this.toppings.push(topping);
+  }
+
+  calculate() {
+    const totalCalories =
+      this.size.calories +
+      this.stuffing.calories +
+      this.toppings.reduce((acc, topping) => acc + topping.calories, 0);
+
+    return totalCalories;
+  }
+
+  calculatePrice() {
+    const totalPrice =
+      this.size.price +
+      this.stuffing.price +
+      this.toppings.reduce((acc, topping) => acc + topping.price, 0);
+
+    return totalPrice;
+  }
+}
+
+// Приклад роботи коду
+const hamburger = new Hamburger(SIZE_SMALL, STUFFING_CHEESE);
+
+hamburger.addTopping(TOPPING_MAYO);
+console.log("Calories: " + hamburger.calculate());
+console.log("Price: " + hamburger.calculatePrice());
+
+hamburger.addTopping(TOPPING_SPICE);
+console.log("Price with spice: " + hamburger.calculatePrice());
